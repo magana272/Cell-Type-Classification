c9735cd8c82967fa3cfb074a0888214ee3eec8d1
magana272 <magana272@berkeley.edu>
2026-04-16 02:36:14 -0700

    increase transformer batch

--- 4_Transformer.py
+++ 4_Transformer.py
@@ -3,7 +3,7 @@ from allen_brain.models.CellTypeAttention import build_pathway_mask
 from allen_brain.cell_data.cell_dataset import make_dataset
 
 SEED = 42
-BATCH_SIZE = 1024
+BATCH_SIZE = 1024*4
 N_HVG = 0
 DATA_DIR = 'data/10x'
 GMT_PATH = 'data/reactome.gmt'
